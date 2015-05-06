8c888502ffa0b9136fc3e555fd871fba434fbc76
Steven Maguire <steven@stevenmaguire.com>
2015-05-05 21:57:59 -0500

    bash path

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -53,7 +53,7 @@ sudo easy_install Pygments
 
 # change to bash 4 (installed by homebrew)
 BASHPATH=$(brew --prefix)/bin/bash
-sudo echo $BASHPATH >> /etc/shells
+echo $BASHPATH | sudo tee -a /etc/shells
 chsh -s $BASHPATH # will set for current user only.
 echo $BASH_VERSION # should be 4.x not the old 3.2.X
 
