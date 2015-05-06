515f66e625f39784b220f97bedbe7d61ecd688d3
Steven Maguire <steven@stevenmaguire.com>
2015-05-05 21:39:38 -0500

    adding permissions

--- lamp.sh
+++ lamp.sh
@@ -1,11 +1,5 @@
 # Install and configure php, apache, mysql using Homebrew
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `lamp.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ## http://getgrav.org/blog/mac-os-x-apache-setup-multiple-php-versions
 
 #------------------------------
@@ -14,10 +8,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make projects directory and structure
 WEB_ROOT=$HOME'/Projects/php/apps'
-mkdir -p $WEB_ROOT
+sudo mkdir -p $WEB_ROOT
 echo "<?php phpinfo();" > $WEB_ROOT"/index.php"
-mkdir -p $HOME"/.httpd/vhosts"
-touch $HOME"/.httpd/vhosts/default.conf"
+sudo mkdir -p $HOME"/.httpd/vhosts"
+sudo touch $HOME"/.httpd/vhosts/default.conf"
 
 #------------------------------
 # PHP
@@ -33,8 +27,8 @@ brew install php56
 brew cleanup
 
 # Create module path
-mkdir -p /usr/local/lib
-touch /usr/local/lib/libphp5.so
+sudo mkdir -p /usr/local/lib
+sudo touch /usr/local/lib/libphp5.so
 
 #------------------------------
 # Apache
@@ -49,19 +43,19 @@ curl -o /etc/apache2/extra/httpd-vhosts.conf https://raw.githubusercontent.com/s
 cp /etc/apache2/httpd.conf.default /etc/apache2/httpd.conf
 
 # Replace config defaults with custom defaults
-sed -i "s|#LoadModule vhost_alias_module|LoadModule vhost_alias_module|g" /etc/apache2/httpd.conf
-sed -i "s|#LoadModule rewrite_module|LoadModule rewrite_module|g" /etc/apache2/httpd.conf
-sed -i "s|#LoadModule php5_module libexec/apache2/libphp5.so|LoadModule php5_module /usr/local/lib/libphp5.so|g" /etc/apache2/httpd.conf
-sed -i "s|/Library/WebServer/Documents|"$WEB_ROOT"|g" /etc/apache2/httpd.conf
-sed -i "s|#Include /private/etc/apache2/extra/httpd-vhosts.conf|Include /private/etc/apache2/extra/httpd-vhosts.conf|g" /etc/apache2/httpd.conf
+sudo sed -i "s|#LoadModule vhost_alias_module|LoadModule vhost_alias_module|g" /etc/apache2/httpd.conf
+sudo sed -i "s|#LoadModule rewrite_module|LoadModule rewrite_module|g" /etc/apache2/httpd.conf
+sudo sed -i "s|#LoadModule php5_module libexec/apache2/libphp5.so|LoadModule php5_module /usr/local/lib/libphp5.so|g" /etc/apache2/httpd.conf
+sudo sed -i "s|/Library/WebServer/Documents|"$WEB_ROOT"|g" /etc/apache2/httpd.conf
+sudo sed -i "s|#Include /private/etc/apache2/extra/httpd-vhosts.conf|Include /private/etc/apache2/extra/httpd-vhosts.conf|g" /etc/apache2/httpd.conf
 
-sed -i "s|/Library/WebServer/Documents|"$WEB_ROOT"|g" /etc/apache2/extra/httpd-vhosts.conf
-echo "Include $HOME/.httpd/vhosts/*.conf" >> /etc/apache2/extra/httpd-vhosts.conf
+sudo sed -i "s|/Library/WebServer/Documents|"$WEB_ROOT"|g" /etc/apache2/extra/httpd-vhosts.conf
+sudo echo "Include $HOME/.httpd/vhosts/*.conf" >> /etc/apache2/extra/httpd-vhosts.conf
 
 # Add current user to apache group
 #dseditgroup -o edit -a $USER -t user _www
 
-apachectl restart
+sudo apachectl restart
 
 #------------------------------
 # MySQL
