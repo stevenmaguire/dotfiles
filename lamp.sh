3b70fa50a4c26d6b6550ce07d8f3e17ba0f74e39
Steven Maguire <steven@stevenmaguire.com>
2015-05-05 19:14:43 -0500

    vhosts paths

--- lamp.sh
+++ lamp.sh
@@ -12,6 +12,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 WEB_ROOT=$HOME'/Projects/php/apps'
 mkdir -p $WEB_ROOT
 echo "<?php phpinfo();" > $WEB_ROOT"/index.php"
+mkdir -p $HOME"/.http/vhosts"
+touch $HOME"/.http/vhosts/default.conf"
 
 # Install PHP
 brew tap homebrew/homebrew-php
