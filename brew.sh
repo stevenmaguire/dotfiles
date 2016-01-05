3f66ed0b7ddde28258e3877600eccf29e01da148
Steven Maguire <stevenmaguire@gmail.com>
2016-01-04 19:22:22 -0600

    Update for more ruby tooling

--- brew.sh
+++ brew.sh
@@ -46,6 +46,8 @@ brew install homebrew/dupes/screen
 brew install ack
 brew install redis
 brew install beanstalkd
+brew install postgres
+
 #install exiv2
 brew install git
 brew install imagemagick --with-webp
--- ruby.sh
+++ ruby.sh
@@ -1,4 +1,8 @@
 # Install and configure ruby and rails environment
 
 brew install ruby
+brew install rbenv
+brew install ruby-build
+rbenv install 2.2.3
 gem install bundler
+rbenv rehash
