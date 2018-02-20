c44b346f0deeeea41b8573e53f959dd0f16c8af2
Steven Maguire <stevenmaguire@gmail.com>
2018-02-20 09:38:52 -0600

    Update dropbox symlinks

--- dropbox.sh
+++ dropbox.sh
@@ -1,12 +1,21 @@
+##################################################
 # Symlinking all the things
+##################################################z
+
+# ssh keys
 mkdir -p $HOME"/.ssh"
 ln -sf $HOME"/Dropbox/Stevil/ssh/id_rsa" $HOME"/.ssh/id_rsa"
 ln -sf $HOME"/Dropbox/Stevil/ssh/id_rsa.pub" $HOME"/.ssh/id_rsa.pub"
+
+# Sequel Pro
 ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/sequel-pro-favorites.plist" $HOME"/Library/Application Support/Sequel Pro/Data/Favorites.plist"
+
+# Transmit
 # ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/transmit.sqlite" $HOME"/Library/Application Support/Transmit/Favorites/Favorites.sqlite"
 # ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/transmit.xml" $HOME"/Library/Application Support/Transmit/Favorites/Favorites.xml"
-ln -sf $HOME"/Dropbox/Stevil/Workbench/sublime-text-3/Installed Packages" $HOME"/Library/Application Support/Sublime Text 3"
-ln -sf $HOME"/Dropbox/Stevil/Workbench/sublime-text-3/Packages" $HOME"/Library/Application Support/Sublime Text 3"
 
-chmod 400 $HOME"/.ssh/id_rsa"
-chmod 400 $HOME"/.ssh/id_rsa.pub"
+# Sublime Text
+rm -rf $HOME"/Library/Application Support/Sublime Text 3/Installed Packages"
+ln -sf $HOME"/Dropbox/Stevil/Workbench/sublime-text-3/Installed Packages" $HOME"/Library/Application Support/Sublime Text 3"
+rm -rf $HOME"/Library/Application Support/Sublime Text 3/Packages"
+ln -sf $HOME"/Dropbox/Stevil/Workbench/sublime-text-3/Packages" $HOME"/Library/Application Support/Sublime Text 3"
