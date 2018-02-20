f37788053e5bc89e2f2caae2351c8824ee298094
Steven Maguire <stevenmaguire@gmail.com>
2018-02-20 11:17:35 -0600

    Remove transmit connection sync

--- dropbox.sh
+++ dropbox.sh
@@ -8,11 +8,8 @@ ln -sf $HOME"/Dropbox/Stevil/ssh/id_rsa" $HOME"/.ssh/id_rsa"
 ln -sf $HOME"/Dropbox/Stevil/ssh/id_rsa.pub" $HOME"/.ssh/id_rsa.pub"
 
 # Sequel Pro
+rm -rf $HOME"/Library/Application Support/Sequel Pro/Data/Favorites.plist"
 ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/sequel-pro-favorites.plist" $HOME"/Library/Application Support/Sequel Pro/Data/Favorites.plist"
-
-# Transmit
-# ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/transmit.sqlite" $HOME"/Library/Application Support/Transmit/Favorites/Favorites.sqlite"
-# ln -sf $HOME"/Dropbox/Stevil/ssh/favorites/transmit.xml" $HOME"/Library/Application Support/Transmit/Favorites/Favorites.xml"
 
 # Sublime Text
 rm -rf $HOME"/Library/Application Support/Sublime Text 3/Installed Packages"
