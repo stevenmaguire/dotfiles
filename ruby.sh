03185928a88b51195a977fe0522c2e3398b8a17b
Steven Maguire <stevenmaguire@gmail.com>
2018-02-21 12:23:08 -0600

    Refactor ruby version installation

--- ruby.sh
+++ ruby.sh
@@ -4,9 +4,17 @@ brew install rbenv
 brew install ruby-build
 
 # https://www.ruby-lang.org/en/downloads/
-rbenv install 2.2.9
-rbenv install 2.3.6
-rbenv install 2.4.3
-rbenv install 2.5.0
+
+declare -a ruby_versions=(
+    '2.2.9'
+    '2.3.6'
+    '2.4.3'
+    '2.5.0'
+)
+for i in "${ruby_versions[@]}"; do
+    eval "rbenv install $i"
+    eval "rbenv global $i"
+    gem install bundler
+done
 
 rbenv rehash
