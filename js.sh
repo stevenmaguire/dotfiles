143adb50445e478d40941f3cb99e6a9d4063ea68
Steven Maguire <stevenmaguire@gmail.com>
2018-03-01 16:56:13 -0600

    Update js.sh

--- js.sh
+++ js.sh
@@ -2,7 +2,13 @@
 # This installs `npm` too using the recommended installation method
 brew install node
 
+# Make a directory for global installations:
+mkdir ~/.npm-global
+
+# Configure npm to use the new directory path:
+npm config set prefix '~/.npm-global'
+
 #------------------------------
 # Install gulp globally
 #------------------------------
-sudo npm install -g gulp bower
+npm install -g gulp bower
