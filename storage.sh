037d5dba010b8deb7618f98bd7ceea958610dda3
Steven Maguire <stevenmaguire@gmail.com>
2018-07-25 14:24:11 -0500

    Remove mysql installation support temporarily

--- storage.sh
+++ storage.sh
@@ -3,20 +3,28 @@
 #------------------------------
 # MySQL
 #------------------------------
-# Let's install latest MySQL because 
+# Let's install latest MySQL because
 # the latest is ¯\_(ツ)_/¯
-brew install mysql
-mysql.server stop
-MY_DAMN_PASSWORD="root"
-CONF_FILE=".my.cnf"
-touch ~/$CONF_FILE
-cat > ~/$CONF_FILE <<EOF
-[mysqld]
-default-authentication-plugin=mysql_native_password
-EOF
-mysql.server start
-mysql -u root -p$MY_DAMN_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MY_DAMN_PASSWORD';"
-mysql.server stop
+#
+# Homebrew has really screwed the pooch with their mysql
+# shenanigans.
+#
+# brew install mysql
+# brew services stop mysql
+# MY_DAMN_PASSWORD="root"
+# CONF_FILE="$(brew --prefix)/etc/my.cnf"
+# touch $CONF_FILE
+# cat > $CONF_FILE <<EOF
+# # Default Homebrew MySQL server config
+# [mysqld]
+# # Only allow connections from localhost
+# bind-address = 127.0.0.1
+# default-authentication-plugin=mysql_native_password
+# EOF
+# brew services start mysql
+# sleep 3
+# mysql -u root -p$MY_DAMN_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MY_DAMN_PASSWORD';"
+# brew services stop mysql
 
 #------------------------------
 # PostgreSQL
