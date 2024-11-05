d1fa628b9091b41dbe5cf6138e99923ebfda4691
Steven Maguire <stevenmaguire@gmail.com>
2024-11-04 20:12:56 -0600

    Update with new things

--- js.sh
+++ js.sh
@@ -1,15 +1,16 @@
-
 # This installs `npm` too using the recommended installation method
-brew install node
+volta install node
 
-# Make a directory for global installations:
-mkdir ~/.npm-global
+# @todo Determine if volta needs any of this
+#
+# # Make a directory for global installations:
+# mkdir ~/.npm-global
 
-# Configure npm to use the new directory path:
-export NPM_CONFIG_PREFIX=~/.npm-global
+# # Configure npm to use the new directory path:
+# export NPM_CONFIG_PREFIX=~/.npm-global
 
-# Make sure I am the owner of these config directories used by package managers
-mkdir ~/.cache
-mkdir ~/.config
-sudo chown -R $(whoami) ~/.cache
-sudo chown -R $(whoami) ~/.config
+# # Make sure I am the owner of these config directories used by package managers
+# mkdir ~/.cache
+# mkdir ~/.config
+# sudo chown -R $(whoami) ~/.cache
+# sudo chown -R $(whoami) ~/.config
